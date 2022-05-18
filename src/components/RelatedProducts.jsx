a65ea6217544ba112750fcdfba637d0381b7176d
Keegan <leary.keegan@gmail.com>
2022-05-17 18:19:25 -0700

    scaffolding for app

---
+++ src/components/RelatedProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RelatedProducts = () => {
+  return (
+    <div>
+      <h2>Related Products</h2>
+    </div>
+  );
+}
+
+export default RelatedProducts
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+  return (
+    <div>
+      <h2>Reviews</h2>
+    </div>
+  );
+}
+
+export default Reviews
