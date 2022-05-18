5a0f708c43ef5189834d399b1aca2603c45e864b
Dan Mao <maodan091216@gmail.com>
2022-05-18 11:33:07 -0700

    delte all the changes I made this morning

--- server/index.js
+++ server/index.js
@@ -3,13 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const config =require('../config.js');
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.send('Hello World this is the express server!')
 })
-app.get('/')
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`listening at http://localhost:${process.env.SERVER_PORT}`)
 })
