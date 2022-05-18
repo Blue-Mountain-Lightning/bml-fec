a0099f6f2af06ac00595975efbc5a132a2079e19
Keegan <leary.keegan@gmail.com>
2022-05-18 12:34:02 -0700

    created some basic css styles for layouts

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './App.css';
 import ProductList from './ProductList.jsx';
+import './styles/Styles.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page-wrapper">
+
       <ProductList />
       <Link to="/details">Product Details</Link>
     </div>
