c8fccaacafdcc1c7f23cc53183b62e70fc97ec5f
Jordan Sweet <hello@jordandsweet.com>
2022-05-19 11:27:34 -0700

    Set up minimal testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './components/App';
 
-test('renders learn react link', () => {
+test('renders Product Details link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Product Details/);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/ProductDetails.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import ProductDetail from './components/ProductDetail';
+
+test('is able to render the ProductDetails component', () => {
+  render(<ProductDetail />);
+});
