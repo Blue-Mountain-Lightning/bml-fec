55980579d610b094ea5684e3ddadd435ef17932a
Fengji <thewyze9@outlook.com>
2022-05-19 17:15:24 -0400

    edit,

--- src/components/ReviewComponents/OneReview.jsx
+++ src/components/ReviewComponents/OneReview.jsx
@@ -11,9 +11,13 @@ class OneReview extends React.Component {
 
   render() {
     return (
-    <h1>Random text for testing</h1>
-    <p>Die Halle der Gibichungen am Rhein. Dort sitzen Gunther und seine Schwester Gutrune, die reinblütigen Gibichungen, mit ihrem Halbbruder Hagen. Diesen hatte Alberich mit Grimhild, der Gibichungen Mutter, gezeugt (Vgl. Die Walküre, II. Akt, Wotan zu Brünnhilde: „Vom Niblung jüngst vernahm ich die Mär, dass ein Weib der Zwerg bewältigt, des Gunst Gold ihm erzwang: des Hasses Frucht hegt eine Frau, des Neides Kraft kreisst ihr im Schoss“). Das Geschwisterpaar erkennt den verschlagenen Hagen neidlos als Ratgeber an. Listig hält er ihnen vor, noch unvermählt zu sein, und fädelt einen geschickten Plan ein: Für Gunther weiß er ein „Weib“, „das herrlichste der Welt“  Brünnhilde , das indes nur Siegfried vom feuerumloderten Berg holen kann. Dieser aber werde Gunthers Bitte erfüllen, um dafür Gutrune als Ehefrau zu gewinnen. Gutrune mag nicht glauben, dass der „herrlichste Held der Welt“ sie begehren könne. Doch Hagen erinnert an einen Trank: Genösse Siegfried den, vergäße er, „daß je ein Weib ihm genaht“. Die Geschwister stimmen diesem Plan begeistert zu, ohne zu bedenken, welches Weib Siegfried vergäße. In Wahrheit freilich geht es Hagen ausschließlich um den Ring.</p>
+      <div>
+        <h1>Random text for testing</h1>
+        <p>Die Halle der Gibichungen am Rhein. Dort sitzen Gunther und seine Schwester Gutrune, die reinblütigen Gibichungen, mit ihrem Halbbruder Hagen. Diesen hatte Alberich mit Grimhild, der Gibichungen Mutter, gezeugt (Vgl. Die Walküre, II. Akt, Wotan zu Brünnhilde: „Vom Niblung jüngst vernahm ich die Mär, dass ein Weib der Zwerg bewältigt, des Gunst Gold ihm erzwang: des Hasses Frucht hegt eine Frau, des Neides Kraft kreisst ihr im Schoss“). Das Geschwisterpaar erkennt den verschlagenen Hagen neidlos als Ratgeber an. Listig hält er ihnen vor, noch unvermählt zu sein, und fädelt einen geschickten Plan ein: Für Gunther weiß er ein „Weib“, „das herrlichste der Welt“  Brünnhilde , das indes nur Siegfried vom feuerumloderten Berg holen kann. Dieser aber werde Gunthers Bitte erfüllen, um dafür Gutrune als Ehefrau zu gewinnen. Gutrune mag nicht glauben, dass der „herrlichste Held der Welt“ sie begehren könne. Doch Hagen erinnert an einen Trank: Genösse Siegfried den, vergäße er, „daß je ein Weib ihm genaht“. Die Geschwister stimmen diesem Plan begeistert zu, ohne zu bedenken, welches Weib Siegfried vergäße. In Wahrheit freilich geht es Hagen ausschließlich um den Ring.</p>
+      </div>
     );
+
+
   }
 }
 export default OneReview;
