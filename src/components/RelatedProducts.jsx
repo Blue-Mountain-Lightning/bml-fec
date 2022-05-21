8ca2e3478d57c00753bdd76720b1fbccf9a50d52
Jordan Sweet <hello@jordandsweet.com>
2022-05-20 17:30:09 -0700

    Memoize repeated related product info requests

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -1,41 +1,62 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import ProductCard from './ProductCard';
 
-const HEADERS = { headers: { 'Authorization': process.env.REACT_APP_TOKEN } };
+const HEADERS = {headers: {'Authorization': process.env.REACT_APP_TOKEN}};
 
-const RelatedProducts = ({ product }) => {
+const RelatedProducts = ({product}) => {
+  const IDCache = useRef({}); // cache of related id's for a particular product
+  const infoCache = useRef({}); // cache of product infos for previously seen products
+  const relatedProductsCache = useRef({});  // this caches all products that have previous been a related product
+
   let [loaded, setLoaded] = useState(false);
   let [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchRelatedProducts = async () => {
-      if (product.id) {
-        try {
+      // if the current product has already been viewed, then load it's related products object from the cache
+      if (!product.id) { return; };
+      try {
+        let productIDs;
+         if (IDCache.current[product.id]) {
+        // Check the IDCache to see if related ID's are stored for current product
+           productIDs = IDCache.current[product.id];
+         } else {
           // Fetch array of product ID's which are related to the current product
           const relatedProductsURL = `${process.env.REACT_APP_API}products/${product.id}/related`;
           const IdsResponse = await fetch(relatedProductsURL, HEADERS)
-          let productIds = await IdsResponse.json();
-          productIds = Array.from(new Set(productIds));
-
-          // Fetch product information for each related product
-          const productRequests = productIds.map(id => {
-            const url = `${process.env.REACT_APP_API}products/${id}`;
-            return fetch(url, HEADERS);
-          })
-          const productsResponse = await Promise.all(productRequests);
-          const responsePromises = productsResponse.map(response => response.json());
-          const responsesData = await Promise.all(responsePromises);
-
-          // Set related product array to products state
-          setProducts(responsesData);
-          setLoaded(true);
-        } catch (error) {
-          console.log(error)
+          let parsedResponse = await IdsResponse.json();
+          productIDs = Array.from(new Set(parsedResponse));
+          IDCache.current[product.id] = productIDs;
         }
-      }
 
+        // Fetch product information for each related product
+        const productRequests = productIDs.map(id => {
+          if (infoCache.current[id]) {
+            return infoCache.current[id];
+          }
+
+          const url = `${process.env.REACT_APP_API}products/${id}`;
+          return fetch(url, HEADERS);
+        })
+        const productsResponse = await Promise.all(productRequests);
+        const responsePromises = productsResponse.map(response => {
+          try {
+            return response.json();
+          } catch {
+            return response;
+          }
+        });
+        const responsesData = await Promise.all(responsePromises);
+        responsesData.forEach(info => infoCache.current[info.id] = info);
+
+        // Set related product array to products state
+        setProducts(responsesData);
+        setLoaded(true);
+      } catch (error) {
+        console.log(error)
+      }
     };
 
     fetchRelatedProducts();
@@ -45,10 +66,10 @@ const RelatedProducts = ({ product }) => {
     return (
       <div>
         <h2>Related Products</h2>
-        {/* This is just a palceholder style for now */}
-        <div style={{ 'display': 'flex', 'flexDirection': 'row', "gap": "48px" }}>
-          {products.map(p => (<ProductCard key={p.id} product={p} />))}
-        </div>
+          {/* This is just a palceholder style for now */}
+          <div style={{'display': 'flex', 'flexDirection': 'row', "gap": "48px"}}>
+            {products.map(p => (<ProductCard key={p.id} product={p} />))}
+          </div>
       </div>
     );
   }
