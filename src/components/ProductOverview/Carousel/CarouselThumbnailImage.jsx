b0003ceb8d4c72e43fc0d9df3505328813dab418
Keegan <leary.keegan@gmail.com>
2022-05-23 08:52:33 -0700

    remove console logs

--- src/components/ProductOverview/Carousel/CarouselThumbnailImage.jsx
+++ src/components/ProductOverview/Carousel/CarouselThumbnailImage.jsx
@@ -2,34 +2,19 @@ import React, { useEffect, useRef } from "react";
 
 const CarouselThumbnailImage = ({ photoUrl, activeSlideIndex, handleClick, index, alt }) => {
 
-  const thumb = useRef();
+  const thumbnailRef = useRef();
   let className = "carousel-thumbnail-image"
   if (activeSlideIndex === index) {
     className += " current";
   }
 
-  // useEffect(() => {
-  //   const thumbDiv = document.querySelector('.carousel-thumbnail-grid');
-  //   const selectedThumb = document.querySelector('.carousel-thumbnail-image.current');
-  //   if (!thumbDiv) return;
-  //   if (!selectedThumb) return;
-  //   const thumbDivRect = thumbDiv.getBoundingClientRect();
-  //   const selectedThumbRect = selectedThumb.getBoundingClientRect();
-
-  //   thumbDiv.scrollTo(thumbDivRect.x, thumbDivRect.y + selectedThumbRect.y)
-
-  //   console.log('thumbDivRect.y: ', thumbDivRect.y);
-  //   console.log('selectedThumbRect.y: ', selectedThumbRect.y)
-  // })
-
   useEffect(() => {
-    if (!thumb.current.classList.contains('current')) return
+    if (!thumbnailRef.current.classList.contains('current')) return
     const thumbDiv = document.querySelector('.carousel-thumbnail-grid');
     if (!thumbDiv) return;
 
-    const selectedThumbRect = thumb.current.getBoundingClientRect();
+    const selectedThumbRect = thumbnailRef.current.getBoundingClientRect();
     const thumbDivRect = thumbDiv.getBoundingClientRect();
-    console.log(selectedThumbRect.y - selectedThumbRect.height)
 
     thumbDiv.scrollTo({ left: 0, top: selectedThumbRect.y - selectedThumbRect.height, behavior: 'smooth' })
 
@@ -38,7 +23,7 @@ const CarouselThumbnailImage = ({ photoUrl, activeSlideIndex, handleClick, index
 
 
   return (
-    <img ref={thumb} className={className} src={photoUrl} alt={alt} onClick={() => handleClick(index)} />
+    <img ref={thumbnailRef} className={className} src={photoUrl} alt={alt} onClick={() => handleClick(index)} />
   )
 }
 
