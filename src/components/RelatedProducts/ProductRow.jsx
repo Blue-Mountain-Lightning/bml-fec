30d3aaad755325302cff72428c584457419cb858
Jordan Sweet <hello@jordandsweet.com>
2022-05-27 02:14:09 -0700

    Overhaul Related Products and use new component to do the rendering work

---
+++ src/components/RelatedProducts/ProductRow.jsx
@@ -0,0 +1,128 @@
+import { useState, useRef, useEffect, useLayoutEffect } from 'react';
+
+import ProductCard from '../ProductCard';
+import ScrollButton from './ScrollButton';
+
+const CARD_WIDTH = 376;
+
+/*
+This components handles the UI/X of Related Items rows.
+*/
+const ProductRow = ({products, offsetVar}) => {
+  const rowRef = useRef();
+
+  const [offset, setOffset] = useState(0);
+  const [margin, setMargin] = useState(0);
+  const [maxOffset, setMaxOffset] = useState(0);
+  const [prevButton, setPrevButton] = useState(false);
+  const [nextButton, setNextButton] = useState(false);
+
+  const setButtonState = (newOffset) => {
+    newOffset = newOffset || offset;
+    if (newOffset >= maxOffset) {
+      setPrevButton(false);
+    } else {
+      setPrevButton(true);
+    }
+
+    if (newOffset <= -maxOffset) {
+      setNextButton(false);
+    } else {
+      setNextButton(true);
+    }
+  }
+
+  const handleResizeWindow = () => {
+    const { innerWidth } = window;
+
+    let numProducts = products.length;
+    let numCards = Math.floor(innerWidth / 408);
+
+    if (numCards > numProducts) {
+      numCards = numProducts;
+    }
+    if (numCards > 4) {
+      numCards = 4;
+    }
+    if (numCards < 1) {
+      numCards = 1;
+    }
+
+    const cardRowWidth = (numCards * CARD_WIDTH);
+    const newMaxOffset = (products.length - numCards) * CARD_WIDTH / 2;
+
+    setOffset(newMaxOffset);
+    setMargin((innerWidth - cardRowWidth) / 2);
+    setMaxOffset(newMaxOffset);
+    setButtonState(newMaxOffset);
+  }
+
+
+  const handleButtonClick = (event, value) => {
+    let newOffset = offset;
+    if (value === 'previous') {
+      if (offset >= maxOffset) {
+        return;
+      }
+      newOffset += CARD_WIDTH;
+    }
+
+    if (value === 'next') {
+      if (offset <= -maxOffset) {
+        return;
+      }
+
+      newOffset -= CARD_WIDTH;
+    }
+
+    setOffset(newOffset);
+    setButtonState(newOffset);
+  }
+
+  useLayoutEffect(() => {
+    // FLIP animate the movement when the offset state is updated.
+    let element = rowRef.current;
+    const first = element.getBoundingClientRect();
+    document.documentElement.style
+      .setProperty(offsetVar, `${offset}px`);
+    const last = element.getBoundingClientRect();
+    const invert = first.x - last.x;
+    element.style.transform = `translateX(${invert}px)`;
+    element.animate([
+      { transform: `translateX(${invert}px)` },
+      { transform: `translateX(0)` },
+    ], {
+      duration: 300,
+      easing: 'cubic-bezier(0, 0, 0.32, 1)',
+    });
+    setButtonState();
+
+  }, [offset, offsetVar]);
+
+  useEffect(() => {
+    handleResizeWindow();
+    window.addEventListener('resize', handleResizeWindow);
+  }, [products]);
+
+  return (
+    <div className='product-row'>
+      <div className='blank-side blank-left'
+           style={{'width': `${margin}px`}}
+           onClick={(e) => handleButtonClick(e, 'previous')}>
+        <ScrollButton direction={'previous'} active={prevButton}/>
+      </div >
+      <div className='products-list horizontal-shifter' ref={rowRef}>
+        {products.map(product => (
+           <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <div className='blank-side blank-right'
+           style={{'width': `${margin}px`}}
+           onClick={(e) => handleButtonClick(e, 'next')}>
+        <ScrollButton direction={'next'} active={nextButton}/>
+      </div>
+    </div>
+  )
+}
+
+export default ProductRow;
--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,40 +1,20 @@
 import React from 'react';
 import { useState, useEffect, useRef } from 'react';
 
-import ProductCard from '../ProductCard';
-import ScrollButton from './ScrollButton';
-import utilities from './utilities';
+import ProductRow from './ProductRow';
 
-import {IMAGE_WIDTH, IMAGE_GAP} from './utilities.js';
-
-const CARD_WIDTH = IMAGE_WIDTH + IMAGE_GAP;
 const OFFSET_VAR = '--related-products-shift-offset';
 const REQUEST_HEADERS = {headers: {'Authorization': process.env.REACT_APP_TOKEN}};
 
 const RelatedProducts = ({product}) => {
   // refs
   const cache = useRef({});
-  const rowElementRef = useRef([]);
 
   // state
   const [loaded, setLoaded] = useState(false);
   const [products, setProducts] = useState([]);
-  const [viewportWidth, setViewportWidth] = useState(0);
-  const [scrollLimitReached, setScrollLimitReached] = useState({});
-
-  // other assignments
-  const marginWidth = utilities.getMarginWidth(viewportWidth, products.length) + 4;
-
-  const handleResizeWindow = (e) => {
-    const {width} = utilities.getWindowDimensions();
-    setViewportWidth(width);
-  }
 
   useEffect(() => {
-    handleResizeWindow();
-
-    window.addEventListener("resize", handleResizeWindow);
-
     const fetchRelatedProducts = async () => {
       if (!product.id) { return; };
 
@@ -94,73 +74,13 @@ const RelatedProducts = ({product}) => {
     fetchRelatedProducts();
   }, [product])
 
-  const checkScrollLimit = () => {
-    let {CSSValueInteger: currentCSSVarValue, boundary}
-      = utilities.scrollStatus(viewportWidth, OFFSET_VAR, products.length);
-
-    const boundaryMargin = 20; // Because I'm paranoid
-    let scrollLimit = scrollLimitReached;
-    if (currentCSSVarValue >= boundary - boundaryMargin) {
-      scrollLimit.previous = true;
-    } else {
-      scrollLimit.previous = false;
-    }
-
-    if (currentCSSVarValue <= -boundary + boundaryMargin) {
-      scrollLimit.next = true;
-    } else {
-      scrollLimit.next = false;
-    }
-
-    setScrollLimitReached(scrollLimit);
-  }
-
-  const handleButtonClick = (event, direction) => {
-    let {CSSValueInteger, boundary}
-      = utilities.scrollStatus(viewportWidth, OFFSET_VAR, products.length);
-
-    if (CSSValueInteger >= boundary - 20 && direction === 'previous') {
-      return;
-    }
-
-    if (CSSValueInteger <= -boundary + 20 && direction === 'next') {
-      return;
-    }
-
-
-    const element = rowElementRef.current;
-    const offset = (direction === 'previous') ? CARD_WIDTH : -CARD_WIDTH;
-    utilities.animatedHorizontalShift(
-      element,
-      OFFSET_VAR,
-      offset
-    );
-
-    checkScrollLimit();
-  }
-
 
   if (loaded) {
     if (products.length === 0) { return <></> };
-    utilities.setInitialOffset(viewportWidth, OFFSET_VAR, products.length);
     return (
       <>
         <h1 className='center-heading'>You may also like</h1>
-          <div className='product-row'>
-            <div className='blank-side blank-left'
-                 style={{'width': `${marginWidth}px`}}
-                 onClick={(e) => handleButtonClick(e, 'previous')}>
-              <ScrollButton direction={'previous'} isActive={scrollLimitReached.previous} />
-            </div >
-          <div className='products-list horizontal-shifter' ref={rowElementRef}>
-            {products.map(product => <ProductCard key={product.id} product={product} />)}
-          </div>
-          <div className='blank-side blank-right'
-               style={{'width': `${marginWidth}px`}}
-               onClick={(e) => handleButtonClick(e, 'next')}>
-            <ScrollButton direction={'next'} isActive={scrollLimitReached.next} />
-          </div>
-        </div>
+        <ProductRow products={products} offsetVar={OFFSET_VAR} />
       </>
     );
   }
