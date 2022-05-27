902006fc28d30edc32aa9c075bb427a2c27e5dd6
Fengji <thewyze9@outlook.com>
2022-05-27 14:09:59 -0400

    added star ratings

--- src/components/ReviewComponents/Stars.jsx
+++ src/components/ReviewComponents/Stars.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Stars.css';
 import { BsStar, BsStarFill } from "react-icons/bs";
+
 const Stars = (props) => {
 
   const [rating, setRating] = useState(0);
